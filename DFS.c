d5c150f7b4f133d3e1ab985b7328abba06c7d1be
BeatriceMathewCEC <chn25mca018@ceconline.edu>
2025-11-06 15:36:49 +0530

    Add files via upload
    06/11/25

---
+++ DFS.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void DFS(int x);
+int G[10][10],visited[10],n;
+void main()
+{
+int i,j;
+printf("Enter the number of vertices: ");
+scanf("%d",&n);
+printf("Enter the adjacency matrix of graph: \n");
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<n;j++)
+{
+scanf("%d",&G[i][j]);
+}
+}
+printf("DFS traversal: ");
+for(i=0;i<n;i++)
+{
+visited[i]=0;
+}
+DFS(0);
+}
+void DFS(int i)
+{
+int j;
+printf("%d",i);
+visited[i]=1;
+for(j=0;j<n;j++)
+{
+if(!visited[j]&&G[i][j]==1)
+{
+DFS(j);
+}
+}
+}
